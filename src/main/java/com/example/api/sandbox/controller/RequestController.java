743a57a9e6ad2e8fc6b5b7d9a1cf85fd24e29900
jonbuck16 <jonbuck16@jonathans-mbp>
2019-09-30 10:16:49 +0100

    Removed unused import

--- src/main/java/com/example/api/sandbox/controller/RequestController.java
+++ src/main/java/com/example/api/sandbox/controller/RequestController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.dizitart.no2.Nitrite;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
